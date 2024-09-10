4d5378cb66ca80af0ffe567a6fceecbcef3a0bf6
Jzy <jestalycastillo15@gmail.com>
2024-09-10 18:45:53 +0800

    kata completed

--- DSA20/Main.java
+++ DSA20/Main.java
@@ -3,18 +3,25 @@ package DSA20;
 public class Main {
 
     public static String breakCamelCase(String input) {
-        for (int i = 0; i < input.length(); i++) {
-            if (Character.toString(input.charAt(i)).equals(Character.toString(input.charAt(i)).toUpperCase())) {
-                System.out.println(input.replace(Character.toString(input.charAt(i - 1)), " "));
 
+        String temp = "";
+        String space;
+
+        for (int i = 0; i < input.length(); i++) {
+            space = "";
+            if (Character.toString(input.charAt(i)).equals(Character.toString(input.charAt(i)).toUpperCase())) {
+                space = " ";
             }
+            temp += space + Character.toString(input.charAt(i));
+
         }
-        return "";
+
+        return input = temp;
     }
 
     public static void main(String args[]) {
-        String input = "camelCasing";
+        String input = "camelCasingArgument";
 
-        breakCamelCase(input);
+        System.out.println(breakCamelCase(input));
     }
 }
