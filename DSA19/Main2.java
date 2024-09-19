91e0d03927f650dca16e000c90b6dbb800f0194b
Jzy <jestalycastillo15@gmail.com>
2024-09-19 23:24:01 +0800

    added exit for while loop

--- DSA19/Main2.java
+++ DSA19/Main2.java
@@ -3,22 +3,33 @@ package DSA19;
 public class Main2 {
 
     public static String order(String words) {
+
         int numCounter = 1;
         int counter = 0;
         String indivWord = "";
         String updatedWord = "";
-        while (counter < words.length()) {
 
+        words = " " + words + " ";
+
+        while (counter < words.length()) {
+            if (counter == words.length() - 1) {
+                if (updatedWord.contains("6")) {
+                    System.out.println("break");
+                    break;
+                }
+                counter = 0;
+            }
             indivWord += Character.toString(words.charAt(counter));
             if (words.charAt(counter) == ' ') {
-
                 if (indivWord.contains(Integer.toString(numCounter))) {
                     updatedWord += indivWord;
                     numCounter++;
-                    // if (counter == words.length() - 1) {
-                    // counter = 0;
-                    // }
                     indivWord = "";
+                    // if (updatedWord.length() == words.length()) {
+                    // System.out.println("break");
+                    // break;
+                    // }
+
                 } else {
                     indivWord = "";
                 }
@@ -27,12 +38,13 @@ public class Main2 {
             counter++;
 
         }
-        return updatedWord;
+        words = updatedWord;
+        return words;
 
     }
 
     public static void main(String args[]) {
-        String words = "3a Thi1s is2 T4est ";
+        String words = "4of Fo1r pe6ople g3ood th5e the2";
         System.out.println(order(words));
     }
 }
