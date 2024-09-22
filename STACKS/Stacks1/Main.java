bd69caf90ad11f89a20091bca9665a951266d447
Jzy <jestalycastillo15@gmail.com>
2024-09-22 16:10:31 +0800

    added stacks

--- STACKS/Stacks1/Main.java
+++ STACKS/Stacks1/Main.java
@@ -1,7 +1,24 @@
 package STACKS.Stacks1;
 
+import java.util.Stack;
+
 public class Main {
     public static void main(String args[]) {
+        // Write a Java program to implement a stack with push and pop operations. Find
+        // the top element of the stack and check if it is empty or not.
 
+        Stack<Integer> stack = new Stack<Integer>();
+
+        stack.push(34);
+        stack.push(22);
+        stack.push(512);
+        stack.push(100);
+        stack.push(2);
+
+        while (stack.peek() <= 100) {
+
+            stack.pop();
+        }
+        System.out.println(stack.peek());
     }
 }
