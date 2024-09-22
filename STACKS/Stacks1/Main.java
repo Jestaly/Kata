776be13abc148b8380e147572ce883160cd99a76
Jzy <jestalycastillo15@gmail.com>
2024-09-22 17:36:13 +0800

    used stack operations in arrays

--- STACKS/Stacks1/Main.java
+++ STACKS/Stacks1/Main.java
@@ -1,24 +1,14 @@
 package STACKS.Stacks1;
 
-import java.util.Stack;
-
 public class Main {
     public static void main(String args[]) {
+        Stack stack = new Stack(5);
+
+        stack.push(20);
+        stack.peek();
+        System.out.println(stack.isEmpty());
         // Write a Java program to implement a stack with push and pop operations. Find
         // the top element of the stack and check if it is empty or not.
 
-        Stack<Integer> stack = new Stack<Integer>();
-
-        stack.push(34);
-        stack.push(22);
-        stack.push(512);
-        stack.push(100);
-        stack.push(2);
-
-        while (stack.peek() <= 100) {
-
-            stack.pop();
-        }
-        System.out.println(stack.peek());
     }
 }
---
+++ STACKS/Stacks1/Stack.java
@@ -0,0 +1,43 @@
+package STACKS.Stacks1;
+
+import java.util.*;
+
+public class Stack {
+
+    // Write a Java program to implement a stack with push and pop operations. Find
+    // the top element of the stack and check if it is empty or not.
+
+    private int[] stackArray;
+    private int counter = 0;
+
+    public Stack(int size) {
+        stackArray = new int[size];
+    }
+
+    public void push(int n) {
+        stackArray[counter] = n;
+        counter++;
+    }
+
+    public void pop() {
+        stackArray[counter - 1] = 0;
+        counter--;
+    }
+
+    public void peek() {
+        System.out.println(Arrays.toString(stackArray));
+
+        if (counter == 0) {
+            System.out.println(stackArray[counter]);
+        } else {
+            System.out.println(stackArray[counter - 1]);
+        }
+    }
+
+    public boolean isEmpty() {
+        if (counter == 0) {
+            return true;
+        }
+        return false;
+    }
+}
