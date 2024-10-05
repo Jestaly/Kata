cfe156f55406df50aa650d9723f23437c9b5a39e
Jzy <jestalycastillo15@gmail.com>
2024-10-05 18:56:48 +0800

    method for String Incrementer

---
+++ DSA/DSA25/Main.java
@@ -0,0 +1,10 @@
+package DSA.DSA25;
+
+public class Main {
+    public static void main(String args[]) {
+        StringIncrementer stringIncrementer = new StringIncrementer();
+        String str = "foo";
+        stringIncrementer.incrementString(str);
+
+    }
+}
---
+++ DSA/DSA25/StringIncrementer.java
@@ -0,0 +1,14 @@
+package DSA.DSA25;
+
+public class StringIncrementer {
+    // foo -> foo1
+    // foobar23 -> foobar24
+    // foo0042 -> foo0043
+    // foo099 -> foo100
+    public String incrementString(String str) {
+        for (int i = 0; i < str.length(); i++) {
+
+        }
+        return str;
+    }
+}
