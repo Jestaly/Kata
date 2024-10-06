da14593f7f39eb89fdf9b4631babf8294cc9d07c
Jzy <jestalycastillo15@gmail.com>
2024-10-06 18:38:29 +0800

    updated stack exercise

--- STACKS/Stacks4/Main.java
+++ STACKS/Stacks4/Main.java
@@ -7,11 +7,9 @@ public class Main {
         Stack<Character> stack = new Stack<Character>();
         String str = "({[]})";
         boolean isBalance = false;
+        
         for (int i = 0; i < str.length(); i++) {
             stack.push(str.charAt(i));
-            if (str.charAt(i)) {
-
-            }
         }
     }
 }
