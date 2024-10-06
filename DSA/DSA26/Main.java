9a2c449ccec21db89f44a93e36ea8ff33cf601c1
Jzy <jestalycastillo15@gmail.com>
2024-10-06 18:38:45 +0800

    created main class for solution

---
+++ DSA/DSA26/Main.java
@@ -0,0 +1,12 @@
+package DSA.DSA26;
+
+public class Main {
+    public static void main(String args[]) {
+        Solution solution = new Solution();
+
+        String s = "ab";
+        String t = "a";
+
+        System.out.println(solution.isAnagram(s, t));
+    }
+}
