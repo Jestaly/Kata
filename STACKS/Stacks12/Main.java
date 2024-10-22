71b30f5d207163ce7648ab0ecdab06c8702269be
Jzy <115347923+Jestaly@users.noreply.github.com>
2024-10-22 14:38:20 +0800

    updated infix to postfix

--- STACKS/Stacks12/Main.java
+++ STACKS/Stacks12/Main.java
@@ -6,22 +6,25 @@ public class Main {
     public static void main(String args[]) {
         Stack<String> stack = new Stack<>();
         String str = "A+(B/C-D)*E";
+        // str = "ABC/D-E*+"
         String strTemp = "";
 
         for (int i = 0; i < str.length(); i++) {
             if (!(str.charAt(i) == '*' || str.charAt(i) == '/' ||
-                    str.charAt(i) == '+' || str.charAt(i) == '-')) {
+                    str.charAt(i) == '+' || str.charAt(i) == '-' ||
+                    str.charAt(i) == '(' || str.charAt(i) == ')')) {
                 strTemp += str.charAt(i);
             } else {
-                if (stack.isEmpty()) {
+                if (!(str.charAt(i - 1) == '*' || str.charAt(i - 1) == '/' ||
+                        str.charAt(i - 1) == '+' || str.charAt(i - 1) == '-') ||
+                        str.charAt(i) == '(' || stack.isEmpty()) {
                     stack.push(Character.toString(str.charAt(i)));
                     continue;
                 }
-
-                if (!(str.charAt(i - 1) == '*' || str.charAt(i - 1) == '/' ||
-                        str.charAt(i - 1) == '+' || str.charAt(i - 1) == '-')) {
-
+                if (str.charAt(i) == ')') {
+                    stack.pop();
                 }
+
             }
         }
     }
