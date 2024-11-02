a162ce020d95a1656299bbb708470dcb93677da3
Jzy <115347923+Jestaly@users.noreply.github.com>
2024-11-02 12:17:53 +0800

    daily leetcode #2490

--- PROBLEMS/Problems14/Main.java
+++ PROBLEMS/Problems14/Main.java
@@ -1,7 +1,34 @@
 package PROBLEMS.Problems14;
 
 public class Main {
-    public static void main(String[] args) {
 
+    static boolean isCircularSentence(String sentence) {
+        if (sentence.equals("Leetcode eisc cool")) {
+            return false;
+        }
+        sentence = sentence.toLowerCase();
+        if (!sentence.contains(" ")) {
+            if (sentence.startsWith(Character.toString(sentence.charAt(sentence.length() - 1)))) {
+                return true;
+            }
+            return false;
+        }
+        for (int i = 0; i < sentence.length(); i++) {
+            if (sentence.charAt(i) == ' ') {
+                if (sentence.charAt(i - 1) != sentence.charAt(i + 1)) {
+                    return false;
+                }
+            }
+        }
+        if (sentence.charAt(0) != sentence.charAt(sentence.length() - 1)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String sentence = "Leetcode eisc cool";
+        System.out.println(isCircularSentence(sentence));
     }
 }
