7d5c6b36719e78c930e7f795727a0dbd08da6eea
Jzy <jestalycastillo15@gmail.com>
2024-11-04 13:48:39 +0800

    daily leetcode #3163 (initial commit)

---
+++ PROBLEMS/Problem17/Main.java
@@ -0,0 +1,12 @@
+package PROBLEMS.Problem17;
+
+public class Main {
+    public static String compressedString(String word) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        String word = "abcde";
+        System.out.println(compressedString(word));
+    }
+}
