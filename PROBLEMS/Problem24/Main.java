0eaeb227e59b9c3d3a468116d7aef0526abd9725
Jzy <115347923+Jestaly@users.noreply.github.com>
2024-11-07 11:22:40 +0800

    daily leetcode #2275 initial

---
+++ PROBLEMS/Problem24/Main.java
@@ -0,0 +1,20 @@
+package PROBLEMS.Problem24;
+
+public class Main {
+    public static int largestCombinationp(int[] candidates) {
+        // System.out.println(Integer.bitCount(5));
+        // System.out.println(Integer.toBinaryString(5));
+
+        int result = 5 & 9;
+        System.out.println(result);
+        // System.out.println(Integer.bitCount(3));
+        // System.out.println(Integer.toBinaryString(3));
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] candidates = { 16, 17, 71, 62, 12, 24, 14 };
+
+        System.out.println(largestCombinationp(candidates));
+    }
+}
