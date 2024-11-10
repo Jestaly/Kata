2115b6ba5ad8038a7fe9c77fd5df52bee9bd6010
Jzy <jestalycastillo15@gmail.com>
2024-11-11 01:07:19 +0800

    initializing code

--- PROBLEMS/Problem26/Main.java
+++ PROBLEMS/Problem26/Main.java
@@ -2,11 +2,15 @@ package PROBLEMS.Problem26;
 
 public class Main {
     int getLucky(String s, int k) {
+
         return k;
 
     }
 
     public static void main(String[] args) {
+        Main main = new Main();
 
+        String s = "";
+        main.getLucky(null, 0);
     }
 }
