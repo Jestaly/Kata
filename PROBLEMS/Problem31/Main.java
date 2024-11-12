45a233e0cbfc2c227b05c549fc9b31fb791a16fb
Jzy <jestalycastillo15@gmail.com>
2024-11-12 22:40:28 +0800

    daily leetcode #262 initial

--- PROBLEMS/Problem31/Main.java
+++ PROBLEMS/Problem31/Main.java
@@ -7,29 +7,27 @@ public class Main {
         if (s.length() != t.length()) {
             return false;
         }
-        int count = 0;
+        StringBuilder temp = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
-            for (int j = 0; j < s.length(); j++) {
-
+            if (s.charAt(i) != t.charAt(i)) {
+                for (int j = 0; j < s.length(); j++) {
+                    if (t.charAt(j) == s.charAt(i)) {
+                        temp.append(t.charAt(j));
+                        t = t.substring(0, j) + " " + t.substring(j + 1, t.length());
+                        break;
+                    }
+                }
+            }
+            if (s.charAt(i) == t.charAt(i)) {
+                temp.append(t.charAt(i));
             }
         }
-        // StringBuilder temp = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            if (!s.contains(Character.toString(t.charAt(i)))) {
-                return false;
-            }
+        if (!temp.toString().equals(s)) {
+            return false;
         }
         return true;
     }
-    // HashMap<String, String> map = new HashMap<>();
-    // map.put(s, t);
-    // System.out.println(map.containsValue(t));
 
-    // StringBuilder temp = new StringBuilder();
-
-    // if (!temp.toString().contains(Character.toString(t.charAt(i)))) {
-    // temp.append(t.charAt(i));
-    // }
     public static void main(String[] args) {
         Main main = new Main();
 
